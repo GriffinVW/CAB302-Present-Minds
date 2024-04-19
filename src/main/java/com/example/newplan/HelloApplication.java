7d8890ba06561df40d3a39fea564013fbba54d8e
Ben Jury <94222105+Otamarau@users.noreply.github.com>
2024-04-19 15:57:52 +1000

    Implemented The UI

--- src/main/java/com/example/newplan/HelloApplication.java
+++ src/main/java/com/example/newplan/HelloApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Settings.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 400);
+//        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        stage.setTitle("Present-Minds!");
         stage.setScene(scene);
         stage.show();
     }
