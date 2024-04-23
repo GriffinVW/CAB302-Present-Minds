b43f366dc342035ee5da8810ad673ff892b0498e
GriffinVW <griffinvannwildman@gmail.com>
2024-04-23 15:31:26 +1000

    added DatabaseConnection class and templates for DAO classes

--- src/main/java/com/example/newplan/EventDAO.java
+++ src/main/java/com/example/newplan/EventDAO.java
@@ -4,10 +4,10 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserDAO {
+public class EventDAO {
     private Connection connection;
 
-    public UserDAO() {
+    public EventDAO() {
         connection = DatabaseConnection.getInstance();
     }
 
@@ -15,11 +15,11 @@ public class UserDAO {
         // Todo Later: Create a Statement to run the CREATE TABLE query
     }
 
-    public void insert(User user) {
+    public void insert(Event event) {
         // Todo Later: Create a PreparedStatement to run the INSERT query
     }
 
-    public void update(User user) {
+    public void update(Event event) {
         // Todo Later: Create a PreparedStatement to run the UPDATE query
     }
 
@@ -27,14 +27,14 @@ public class UserDAO {
         // Todo Later: Create a PreparedStatement to run the DELETE query
     }
 
-    public List<User> getAll() {
-        List<User> users = new ArrayList<>();
+    public List<Event> getAll() {
+        List<Event> events = new ArrayList<>();
         // Todo Later: Create a Statement to run the SELECT * query
         // and populate the accounts list above
-        return users;
+        return events;
     }
 
-    public User getById(int id) {
+    public Event getById(int id) {
         // Todo Later: Create a PreparedStatement to run the conditional SELECT query
         return null;
     }
