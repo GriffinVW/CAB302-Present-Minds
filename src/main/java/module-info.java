6f2235c5a427df650e2b1b1abafa67903f57b308
Fluffy-Dinosaur <tchee24@hotmail.com>
2024-04-23 15:24:27 +1000

    Added user infomation handler

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.newplan {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
     requires org.kordamp.bootstrapfx.core;
 
     opens com.example.newplan to javafx.fxml;
