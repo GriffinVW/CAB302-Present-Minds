973a549eee3554bf69affbc5f9d472ea4a5542be
GriffinVW <griffinvannwildman@gmail.com>
2024-04-26 18:30:41 +1000

    added deleted files again with code

--- src/main/java/com/example/newplan/model/MockUserData.java
+++ src/main/java/com/example/newplan/model/MockUserData.java
@@ -1,5 +1,21 @@
 package com.example.newplan.model;
 
+import java.util.ArrayList;
+
 //deleted
+
 public class MockUserData {
+
+    public static final ArrayList<User> user_data = new ArrayList<>();
+    private static int autoIncrementedId = 0;
+
+    public MockUserData() {
+        //addUserData(new User("John", "Doe", "JD@gmail.com", "1234"));
+    }
+
+    public void addUserData(User userdata) {
+        userdata.setId(autoIncrementedId);
+        autoIncrementedId++;
+        user_data.add(userdata);
+    }
 }
