431788c7e8170e1cb49a51c17e347b2a1c627275
Loman <smitloman@gmail.com>
2024-04-30 14:06:20 +1000

    Added Test Directory and finished Tests For Event class

--- src/main/java/com/example/newplan/Event.java
+++ src/main/java/com/example/newplan/Event.java
@@ -28,13 +28,14 @@ public class Event {
         this.title = title;
         this.description = description;
         //following require conversions
-        Timestamp timestamp = Timestamp.valueOf(startTime);
+        Timestamp timestamp1 = Timestamp.valueOf(startTime);
         Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(timestamp.getTime());
+        calendar.setTimeInMillis(timestamp1.getTime());
         this.startTime = calendar;
-        timestamp = Timestamp.valueOf(endTime);
-        calendar.setTimeInMillis(timestamp.getTime());
-        this.endTime = calendar;
+        Calendar calendar1 = Calendar.getInstance();
+        Timestamp timestamp2 = Timestamp.valueOf(endTime);
+        calendar1.setTimeInMillis(timestamp2.getTime());
+        this.endTime = calendar1;
         this.restrict = restrict.equals("1");
         this.reminder = reminder.equals("1");
     }
@@ -55,14 +56,17 @@ public class Event {
     public String getTitle() {
         return title;
     }
+    public void setTitle(String title) {this.title = title;}
 
     public String getDescription() {
         return description;
     }
-
+    public void setDescription(String description) {this.description = description;}
     public Calendar getStartTime() {
         return startTime;
     }
+
+    public void setStartTime(Calendar cal){this.startTime = cal;}
 
     // converts Calender to a timeStamp then into a string.
 
@@ -71,9 +75,13 @@ public class Event {
         return (timestamp1.toString());
     }
 
+
     public Calendar getEndTime() {
         return endTime;
     }
+
+    public void setEndTime(Calendar cal){this.endTime = cal;}
+
 
     public String getEndTimeStr() {
     Timestamp timestamp1 = new Timestamp(endTime.getTimeInMillis());
@@ -86,11 +94,12 @@ public class Event {
     public String  getRestrictBit() {
         if(restrict){return "1";}
         else {return "0";}
-
     }
+    public void setRestrict(Boolean restrict){this.restrict = restrict;}
     public boolean getReminder() {
         return reminder;
     }
+    public void setReminder(Boolean reminder){this.reminder = reminder;}
     public String getReminderBit() {
         if(reminder){return "1";}
         else {return "0";}
